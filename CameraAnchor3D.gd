67e61981d18dbaae7e749b1a74320e87b017239f
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2022-06-25 09:06:41 +0200

    Better camera movement

--- CameraAnchor3D.gd
+++ CameraAnchor3D.gd
@@ -9,26 +9,29 @@ func _input(event):
 	var viewport = self.get_viewport()
 	var margin_w = viewport.size.x / 15
 	var margin_h = viewport.size.y / 15
-	var move_x = 0
-	var move_z = 0
+	var move = Vector3(0, 0, 0)
+	var camera_forward = get_global_transform().basis.z
+	var camera_left = get_global_transform().basis.x
 	# Should move one screen width every 5 seconds
-	var move_vel = viewport.size.x / 30
+	var move_vel = $Camera.size
 	if event is InputEventMouseMotion:
 		if event.position.x < margin_w:
-			move_x = 1
+			move += camera_left
 		elif event.position.x > viewport.size.x - margin_w:
-			move_x = -1
-		else:
-			move_x = 0
+			move -= camera_left
 
 		if event.position.y < margin_h:
-			move_z = 1
+			move += camera_forward
 		elif event.position.y > viewport.size.y - margin_h:
-			move_z = -1
-		else:
-			move_z = 0
+			move -= camera_forward
 		
-		self.velocity = Vector3(move_x * move_vel, 0, move_z * move_vel)
+		self.velocity = move.normalized() * move_vel
+	elif event is InputEventMouseButton:
+		if event.is_pressed():
+			if event.button_index == BUTTON_WHEEL_UP:
+				$Camera.size /= 2
+			elif event.button_index == BUTTON_WHEEL_DOWN:
+				$Camera.size *= 2
 
 func _physics_process(_delta):
 	self.move_and_slide(self.velocity)
