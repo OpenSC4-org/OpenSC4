6f3cf4cdc5782b9171a2ca4097576c5cb7510df4
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2022-08-20 17:15:32 +0200

    Display text with GZWinText

--- addons/dbpf/GZWinText.gd
+++ addons/dbpf/GZWinText.gd
@@ -1,7 +1,20 @@
 extends GZWin
 class_name GZWinText
 
-var text : String = ""
+var text : String 
+var font = null
 
 func _init(attributes).(attributes):
-	self.text = attributes.get('caption', '')
+	self.set_text(attributes.get('caption', ''))
+	# hack to get the default font while we can't decode the Simcity 4 ones
+	var label = Label.new()
+	self.font = label.get_font("")
+	label.free()
+
+func set_text(text : String):
+	self.text = text
+	self.update()
+
+func _draw():
+	draw_string(font, Vector2(0, font.get_height()), self.text, Color.white)
+
