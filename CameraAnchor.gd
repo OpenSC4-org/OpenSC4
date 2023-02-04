9678577b85b3074be60d822a5a5ec3304ce06374
Olivia May <olmay@tuta.io>
2023-02-03 20:17:36 -0600

    Camera optimization

--- CameraAnchor.gd
+++ CameraAnchor.gd
@@ -5,7 +5,7 @@ var viewport = 0
 var margin_w = 0
 var margin_h = 0
 var move = Vector2(0, 0)
-#dk Should move one screen width every 4 seconds
+#Should move one screen width every 4 seconds
 #var move_speed = viewport.size.x / 4
 var move_speed = 16834 # just manually setting it idk
 
@@ -19,22 +19,22 @@ func _input(event):
 	
 	if event is InputEventMouseMotion:
 		if event.position.x < margin_w:
-			move.x = -move_speed
+			move.x = -1
 		elif event.position.x > viewport.size.x - margin_w:
-			move.x = move_speed
+			move.x = 1
 
 		if event.position.y < margin_h:
-			move.y = -move_speed
+			move.y = -1
 		elif event.position.y > viewport.size.y - margin_h:
-			move.y = move_speed
+			move.y = 1
 	if Input.is_action_pressed("camera_up"):
-		move.y = -move_speed
+		move.y = -1
 	if Input.is_action_pressed("camera_down"):
-		move.y = move_speed
+		move.y = 1
 	if Input.is_action_pressed("camera_left"):
-		move.x = -move_speed
+		move.x = -1
 	if Input.is_action_pressed("camera_right"):
-		move.x = move_speed
+		move.x = 1
 	
 	# `normalized() * move_speed` so no fast diagonal movement
 	self.velocity = Vector2(move.x, move.y).normalized() * move_speed
