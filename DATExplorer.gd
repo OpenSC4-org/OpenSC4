e63897689cc16d6e56259b2d48e57f38912c9eb0
Adrien Jaguenet <adrien_jaguenet@makelofine.org>
2023-02-06 11:27:57 +0100

    Choose a directory with the DAT explorer

--- DATExplorer.gd
+++ DATExplorer.gd
@@ -6,11 +6,10 @@ var tree : Tree
 
 var DIRECTORY = "res://.dev/ui/Game/"
 
-func _init():
-	DIRECTORY = "res://"
-	# Find all .dat files in the .dev directory
+func load_dats():
 	var dir = Directory.new()
 	var err = dir.open(DIRECTORY)
+	Logger.info("Opening directory %s" % DIRECTORY)
 	if err != OK:
 		print('Error opening dev directory: %s' % err)
 		return
@@ -23,28 +22,34 @@ func _init():
 			self.dbpf_files_src.append(file)
 	dir.list_dir_end()
 
+	Logger.info("Found %d .dat files" % [self.dbpf_files_src.size()])
 	# Load all .dat files
 	for file in self.dbpf_files_src:
 		var dbpf = DBPF.new('%s%s' % [DIRECTORY, file])
 		dbpf_files.append(dbpf)
+	reload_no_filter()
 
 func _ready():
 	tree = $DATTree
 	$TypeFilter/FilterByTypeID.connect("pressed", self, "filter_by_type_id")
 	$InstanceFilter/FilterByInstanceID.connect("pressed", self, "filter_by_instance_id")
+	$DirectoryChoice.popup()
 	reload_filtered_by_type_id(0x0)
 
 func reload_no_filter():
 	$DATTree.clear()
 	var root = tree.create_item()
 	tree.set_hide_root(true)
+	var count = 0
 	for file in self.dbpf_files:
 		var file_child = tree.create_item(root)
 		file_child.set_text(0,file.path)
 		if file.indices_by_type.has(0):
 			for subfile in file.indices.values():
+				count += 1
 				var subfile_child = tree.create_item(file_child)
 				subfile_child.set_text(0,SubfileTGI.get_file_type(subfile.type_id, subfile.group_id, subfile.instance_id))
+	Logger.info("Found %d files" % [count])
 
 func reload_filtered_by_type_id(type_id):
 	$DATTree.clear()
@@ -87,3 +92,10 @@ func filter_by_instance_id():
 	var value = $InstanceFilter/InstanceIDFilter.text.hex_to_int()
 	print("Filtering instance ID = 0x%08x" % value)
 	reload_filtered_by_instance_id(value)
+
+func _on_DirectoryChoice_dir_selected(dir: String):
+	DIRECTORY = dir 
+	$TypeFilter.visible = true
+	$InstanceFilter.visible = true
+	load_dats()
+
